5a9c9e266a936900962eedcebc9097e8def32fad
vats199 <vatsalp.tcs@gmail.com>
2022-06-03 09:35:33 +0530

    creating signup API

---
+++ source/controllers/authController.ts
@@ -0,0 +1,11 @@
+import User from "../models/userModel";
+import Token from "../models/tokenModel";
+
+import crypto from "crypto"
+import bcrypt from "bcryptjs"
+
+import { Request, Response } from "express";
+
+const Signup = (req: Request, res: Response) => {
+    
+}
